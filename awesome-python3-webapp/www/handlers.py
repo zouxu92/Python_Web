175e772bdbfa059e7452c972e72198a6c93c3ea1
zouxu9203 <zouxu9203@163.com>
2016-05-08 17:04:37 +0800

    提交登录注册html与请求函数调用‘

--- awesome-python3-webapp/www/handlers.py
+++ awesome-python3-webapp/www/handlers.py
@@ -2,19 +2,16 @@
 
 import re, time, json, logging, hashlib, base64, asyncio
 
+import markdown2
+
+from aiohttp import web
+
 from coroweb import get, post
+from apis import APIValueError, APIResourceNotFoundError
 
 from models import User, Comment, Blog, next_id
+from config import configs
 
-'''
-@get('/')
-async def index(request):
-    users = await User.findAll()
-    return {
-        '__template__': 'test.html',
-        'users': users
-    }
-'''
 # 首页Url的处理函数更新
 @get('/')
 def index(request):
@@ -29,6 +26,12 @@ def index(request):
         '__template__': 'blogs.html',
         'blogs': blogs
     }
+# 注册页面调用
+@get('/register')
+def register():
+    return {
+        '__template__': 'register.html'
+    }
 
 @get('/api/users')
 def aip_get_users():
@@ -37,17 +40,130 @@ def aip_get_users():
         u.passwd = '******'
     return dict(users=users)
 
+# 登录界面调用
+@get('/signin')
+def signin():
+    return {
+        '__template__': 'signin.html'
+    }
 
-'''
-@get('/api/users')
-def aip_get_users(*, page='1'):
-    page_index = get_page_index(page)
-    num = yield from User.findNumber('count(id)')
-    p = Page(num, page_index)
-    if num == 0:
-        return dict(page=p, users=())
-    users = yield from User.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
-    for u in users:
-        u.passwd = '********'
-    return dict(page=p, users=users)
-'''
+
+
+# 登录API实现
+@post('/api/authenticate')
+def authenticate(*, email, passwd):
+    if not email:
+        raise APIValueError('email', 'Invalid email.')
+    if not passwd:
+        return APIValueError('passwd', 'Invalid password.')
+    users = yield from User.findAll('email=?', [email])
+    if len(users) == 0:
+        raise APIValueError('email', 'Email not exist.')
+    user = users[0]
+    # check passwd:
+    sha1 = hashlib.sha1()
+    sha1.update(user.id.encode('utf-8'))
+    sha1.update(b':')
+    sha1.update(passwd.encode('utf-8'))
+    if user.passwd != sha1.hexdigest():
+        raise APIValueError('passwd', 'Invalid passwd.')
+    # authenticate ok, set cookie:
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    user.passwd = '******'
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+    return r
+
+# 退出登录
+@get('/signout')
+def signout(request):
+    referer = requset.headers.get('Referer')
+    r = web.HTTPFound(referer or '/')
+    r.set_cookie(COOKIE_NAME, '-deleted-', max_age=0, httponly=True)
+    logging.info('user signed out.')
+    return r
+
+# 计算加密cookie：
+def user2cookie(user, max_age):
+    # build cookie string by:id-expires-sha1
+    expires = str(int(time.time() + max_age))
+    s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
+    L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
+    return '-'.join(L)
+
+# 每个Url处理函数，如果都去写解析cookie 的代码，会导致代码重复很多次
+# so，利用middle在处理URL之前，把cookie 解析出来，并将登录用户绑定到request对象上
+@asyncio.coroutine
+def auth_factory(app, handler):
+    @asyncio.coroutine
+    def auth(request):
+        logging.info('check user: %s %s' % (request.method, request.path))
+        request.__user__ = None
+        cookie_str = request.cookies.get(COOKIE_NAME)
+        if cookie_str:
+            user = yield from cookies2user(cookie_str)
+            if user:
+                logging.info('set current user: %s' % user.email)
+                request.__user__ = user
+        return (yield from handler(request))
+    return auth
+
+# 解密cookie：
+@asyncio.coroutine
+def cookie2user(cookie_str):
+    '''
+    Parse cookie and load user if cookie is valid.
+    '''
+    if not cookie_str:
+        return None
+    try:
+        L = cookie_str.split('_')
+        if len(L) != 3:
+            return None
+        uid, expires, sha1 = L
+        if int(expires) < time.time():
+            return None
+        user = yield from User.find(uid)
+        if user is None:
+            return None
+        s = '%s-%s-%s-%s' % (uid, user.passwd, expires, _COOKIE_KEY)
+        if sha1 != hashlib.sha1(s.encode('utf-8')).hexdigest():
+            logging.info('Invalid sha1')
+            return None
+        user.passwd = '******'
+        return user
+    except Exception as e:
+        logging.exception(e)
+        return None
+
+
+
+
+
+
+# 用户注册
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
+_RE_SHAI = re.compile(r'^[0-9a-z]{40}$')
+
+@post('/api/users')
+def api_register_user(*, email, name, passwd):
+    if not name or not name.strip():
+        raise APIValueError('name')
+    if not email or not _RE_EMAIL.match(email):
+        raise APIValueError('email')
+    if not passwd or not _RE_SHAI.match(passwd):
+        raise APIValueError('passwd')
+    users = yield from User.findAll('email=?', [email])
+    if len(users) > 0:
+        raise APIError('register:failed', 'email', 'Email is already in use.')
+    uid = next_id()
+    sha1_passwd = '%s:%s' % (uid, passwd)
+    # 用户口令经过SHA1计算后的40为Hsah字符串.
+    user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(),
+                image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
+    yield from user.save()
+    # make session cookie
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    return r
